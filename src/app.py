ea07b2aa1de8eef79b3ac1ab54846fd553ed529c
wy91-2021 <1669747363@qq.com>
2026-01-13 06:09:53 +0000

    添加活动报名验证，防止重复报名

--- src/app.py
+++ src/app.py
@@ -99,6 +99,8 @@ def signup_for_activity(activity_name: str, email: str):
     activity = activities[activity_name]
 
     # Validate student is not already signed up
+    if email in activity["participants"]:
+        raise HTTPException(status_code=400, detail="Student is already signed up for this activity")
 
     # Add student
     activity["participants"].append(email)
